1f5ac075747706d4ef06ae083cdb3a88cc6fe5d8
Abhijith Selvam <abselvam44@gmail.com>
2026-01-04 19:38:56 +0530

    dashboard page for users added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,19 @@
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainActions from "@/components/dashboard/MainActions";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
 import Navbar from "@/components/Navbar";
-import { SignOutButton } from "@clerk/nextjs";
 import React from "react";
 
 function Dashboard() {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1>Dashboard</h1>
-      <SignOutButton />
-    </div>
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
   );
 }
 
